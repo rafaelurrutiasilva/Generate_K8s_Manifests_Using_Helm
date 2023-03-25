386707a29289b55013080f9c8a6d9d3634f49c7f
rafaelurrutiasilva <Rafael.Urrutia.S@gmail.com>
2023-03-25 13:10:34 +0000

    update script

--- generate_manifests.sh
+++ generate_manifests.sh
@@ -6,16 +6,38 @@
 #
 DEBUG=1
 
+PROGRAM=`basename $0`
+VERSION="2023-03-25, Rafael.Urrutia.S@gmail.com"
+
 # Source initial variables.
 . ./manifests.ini
 
-# Create root-dir if it missing.
-[ -d "$MANIFEST_DIR_NAME" ] || mkdir -p $MANIFEST_DIR_NAME
+checkBasics(){
+	[[ -f $VALUEFILE ]] || echo "You are missing the '$VALUEFILE' file!" 
+	[[ $(which kubeseal 2> /dev/null ) ]] || echo "You have to have 'kubeseal' installed!"
+	[[ $(which helm 2> /dev/null ) ]] || echo "You have to have 'helm' installed!"
+}
+
+printHelp() {
+	clear
+	echo $VERSION
+        echo ""
+        echo "This Bash script generates Kubernetes manifests using Helm and Kubeseal"
+        echo ""
+        echo "Use of $PROGRAM:"
+	echo ""
+	echo "-h, --help		Shows help text"
+	echo "-u, --update (y/n) 	Tells if you are doing an update and will keep existing secrets or want to generate new ones. Default i yes."
+        echo ""
+        exit $STATE_UNKNOWN
+}
+
 
 # Helm workload
 helmWorkload() {
 	[[ $DEBUG -eq 1 ]] && echo "DEBUGING... Running helmWorkload"
 
+	[[ -d "$MANIFEST_DIR_NAME" ]] || mkdir -p $MANIFEST_DIR_NAME
 	helm repo add $HELM_REPO_NAME $HELM_REPO_URL
 	helm repo update
 	helm template $HELM_APP_NAME $HELM_REPO_NAME/$HELM_CHART_NAME --namespace $HELM_APP_NAMESPACE \
@@ -35,5 +57,7 @@ genSealedSecret() {
 }	
 
 # Run functions
-helmWorkload
-genSealedSecret 
+checkBasics
+#printHelp
+#helmWorkload
+#genSealedSecret
